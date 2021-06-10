4161ad215b94b50a1cd2422052b13452a53797fc
Geetika Jana <geetika353@gmail.com>
2021-06-10 20:38:17 +0530

    FirstCommit

---
+++ C#Program1.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class forExample
+{
+   static void Main(string[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+            Console.WriteLine("The value of i is: \n");
+            Console.WriteLine(i);
+        }
+    }
+}
